e3045886b436b47df24e8f5acaf954ee5f7babd8
Stephanie Vartany <60303448+svartany@users.noreply.github.com>
2023-05-19 14:20:14 -0400

    added C's start_trim function for long labels

--- R/utilities.R
+++ R/utilities.R
@@ -54,3 +54,39 @@ my_shinyalert_tryCatch <- function(expr,
     return(return.error)
   })
 }
+
+
+# Intelligently trim labels (e.g. sample labels) to the most unique substring
+# This is useful for plots that may display very long labels
+#' @importFrom stringr str_sub
+smart_trim <- function(labels, trim_length=10, default_trim=c("beginning", "end")) {
+  default_trim = match.arg(default_trim)
+  
+  check_length = trim_length # initialize check_length as the FULL trim_length
+  # until we've run out of trim_lengths to check
+  while (check_length > 0) {
+    unique_at_beginning = length(unique(sapply(labels, function(x) {str_sub(x,1,check_length)} )))
+    unique_at_end = length(unique(sapply(labels, function(x) {str_sub(x,-check_length,-1)} )))
+    
+    if (unique_at_beginning == unique_at_end) { # if we have the same number of unique strings
+      check_length=check_length-1 # decrease the trim-length we're checking
+      next # try again, with a shorter sub_str
+    } else if (unique_at_beginning > unique_at_end) { # if we had more unique strings at the beginning of the string
+      labels_trimmed = sapply(labels, function(x) {str_sub(x,1,trim_length)} ) # trim to the beginning
+      break
+    } else { # if we had more unique strings at the beginning of the string
+      labels_trimmed = sapply(labels, function(x) {str_sub(x,-trim_length,-1)} ) # trim to the unique_at_end
+      break
+    }
+  }
+  
+  # if we 
+  if (check_length==0) {
+    warning(paste0("Labels appear to be equally unique at the beginning and end. Defaulting to ", default_trim, ".\n"))
+    labels_trimmed = switch(default_trim,
+                            "beginning" = sapply(labels, function(x) {str_sub(x,1,trim_length)} ),
+                            "end" = sapply(labels, function(x) {str_sub(x,-trim_length,1)} ))
+  }
+  
+  return(unname(labels_trimmed)) # return the trimmed vectors, unnamed
+}
---
+++ tests/testthat/test-utilities.R
@@ -0,0 +1,23 @@
+
+test_that("smart_trim works on example labels", {
+  # test labels
+  labels = c('xxxx_stuffandthings_xasf',
+             'xxxx_stuffandthings_ejwt',
+             'xxxx_stuffandthings_fdsh')
+  
+  labels_reverse = c('fsax_sgnihtdnaffuts_xxxx',
+                     'twje_sgnihtdnaffuts_xxxx',
+                     'hsdf_sgnihtdnaffuts_xxxx')
+  
+  labels_equal_difference = c('xxxx_stuffandthings_yyyy',
+                              'xxxx_thingsandstuff_yyyy',
+                              'xxxx_stuffandthings_yyyy')
+  
+  
+  expect_equal(suppressWarnings(smart_trim(labels)), 
+               c("hings_xasf", "hings_ejwt", "hings_fdsh"))
+  expect_equal(suppressWarnings(smart_trim(labels_reverse)),
+               c("fsax_sgnih", "twje_sgnih", "hsdf_sgnih"))
+  expect_equal(suppressWarnings(smart_trim(labels_equal_difference)),
+               c("xxxx_stuff", "xxxx_thing", "xxxx_stuff"))
+})
