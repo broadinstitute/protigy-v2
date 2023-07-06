74f7b10ca428d91977cb1d1e490ea234d8ba262b
Stephanie Vartany <60303448+svartany@users.noreply.github.com>
2023-07-06 11:02:21 -0400

    added tests and test data

---
+++ R/data.R
@@ -0,0 +1,63 @@
+# This script documents the test datasets saved in the /data folder
+
+#' Phosphoproteome example GCT
+#' 
+#' A GCT containing the BRCA retrospective v5.0 phosphoproteome dataset.
+#' 
+#' @format ## 'brca_retrospective_v5.0_phosphoproteome_gct'
+#' Formal class 'GCT' from package `cmapR` with 7 slots:
+#' \describe{
+#'   \item{mat}{data matrix}
+#'   \item{rid}{row ids}
+#'   \item{cid}{column ids}
+#'   \item{rdesc}{row description dataframe}
+#'   \item{cdesc}{column description dataframe}
+#'   \item{version}{GCT version}
+#'   \item{src}{GCT source file}
+#' }
+#' 
+#' @source https://github.com/broadinstitute/PANOPLY/blob/dev/tutorial/tutorial-brca-input.zip
+#' 
+"brca_retrospective_v5.0_phosphoproteome_gct"
+
+#' Proteome example GCT
+#' 
+#' A GCT containing the BRCA retrospective v5.0 proteome dataset.
+#' 
+#' @format ## 'brca_retrospective_v5.0_phosphoproteome_gct'
+#' Formal class 'GCT' from package `cmapR` with 7 slots:
+#' \describe{
+#'   \item{mat}{data matrix}
+#'   \item{rid}{row ids}
+#'   \item{cid}{column ids}
+#'   \item{rdesc}{row description dataframe}
+#'   \item{cdesc}{column description dataframe}
+#'   \item{version}{GCT version}
+#'   \item{src}{GCT source file}
+#' }
+#' 
+#' @source https://github.com/broadinstitute/PANOPLY/blob/dev/tutorial/tutorial-brca-input.zip
+#' 
+"brca_retrospective_v5.0_proteome_gct"
+
+#' RNAseq example GCT
+#' 
+#' A GCT containing the BRCA retrospective v5.0 RNAseq dataset.
+#' 
+#' @format ## 'brca_retrospective_v5.0_rnaseq_gct'
+#' Formal class 'GCT' from package `cmapR` with 7 slots:
+#' \describe{
+#'   \item{mat}{data matrix}
+#'   \item{rid}{row ids}
+#'   \item{cid}{column ids}
+#'   \item{rdesc}{row description dataframe}
+#'   \item{cdesc}{column description dataframe}
+#'   \item{version}{GCT version}
+#'   \item{src}{GCT source file}
+#' }
+#' 
+#' @source https://github.com/broadinstitute/PANOPLY/blob/dev/tutorial/tutorial-brca-input.zip
+#' 
+"brca_retrospective_v5.0_rnaseq_gct"
+
+
