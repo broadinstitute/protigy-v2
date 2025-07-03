ecd0aa73a744e4e54e7f2a5a669551c8f2dd3137
dabburi1 <dabburi@broadinstitute.org>
2025-07-03 11:23:48 -0400

    new ui (two sample not completed)

--- R/tab_stat_summary.R
+++ R/tab_stat_summary.R
@@ -12,27 +12,27 @@
 # contains the structure for the big tabbed box with omes
 statSummary_Tab_UI <- function(id = "statSummaryTab") {
   ns <- NS(id) # namespace function, wrap UI inputId's with this `ns("inputId")`
-  
+
   tagList(
-    
+
     # display omes tabs
     fluidRow(uiOutput(ns("ome_tabset_box")))
-    
+
   ) # end tagList
 }
 
 # server for the statSummary tab
 # contains the structure for the big tabbed box with omes
 statSummary_Tab_Server <- function(id = "statSummaryTab",
-                                 GCTs_and_params, 
-                                 globals, 
-                                 GCTs_original) { 
+                                 GCTs_and_params,
+                                 globals,
+                                 GCTs_original) {
 
   ## module function
   moduleServer(id, function (input, output, session) {
-    
+
     ## GATHERING INPUTS ##
-    
+
     # get namespace in case you need to use it in renderUI-like functions
     ns <- session$ns
 
@@ -41,77 +41,77 @@ statSummary_Tab_Server <- function(id = "statSummaryTab",
       validate(need(GCTs_and_params(), "GCTs not yet processed"))
       GCTs_and_params()$GCTs
     })
-    
+
     # parameters used to process GCTs
     parameters <- reactive({
       validate(need(GCTs_and_params(), "GCTs not yet processed"))
       GCTs_and_params()$parameters
     })
-    
+
     # Large merged GCT with all omes containing `protigy.ome` column in `rdesc`
     GCTs_merged <- reactive({
       validate(need(GCTs_and_params(), "GCTs not yet processed"))
       GCTs_and_params()$GCTs_merged
     })
-    
+
     # named list of default annotation columns for each ome
     default_annotations <- reactive({
       req(parameters())
       sapply(parameters(), function(p) p$annotation_column, simplify = FALSE)
     })
-    
+
     # vector of all omes
     all_omes <- reactive(names(GCTs())) # don't remove
-    
+
     # gather relevant variables from globals
     default_ome <- reactive(globals$default_ome) # don't remove this variable!
     custom_colors <- reactive(globals$colors)
-    
-    
-    ## OME TABS ##
-    
+
+
+   ## OME TABS ##
+
     # handles compiling ome tabs into styled tabset box
     output$ome_tabset_box <- renderUI({
-      req(shared$chosen_omes)
-      
+      req(all_omes(), default_ome())
+    
       # generate a tab for each -ome
-      tabs <- lapply(shared$chosen_omes, function(ome){
+      tabs <- lapply(all_omes(), function(ome){
         tabPanel(
           title = ome,
-          
+    
           # call the UI function for each individual ome
-          statSummary_Ome_UI(id = ns(ome), ome = ome)
-          
+          templateSingleOme_Ome_UI(id = ns(ome), ome = ome)
+    
         ) # end tabPanel
       }) # end lapply
-      
+    
       # combine all tabs into tabSetPanel
       tab_set_panel <- do.call(
-        tabsetPanel, 
-        c(tabs, list(id = ns("ome_tabs")))
+        tabsetPanel,
+        c(tabs, list(id = ns("ome_tabs"), selected = isolate(default_ome())))
       )
-      
+    
       # put everything in a big box with ome tabs and return
       # add necessary CSS classes
       add_css_attributes(
         shinydashboardPlus::box(
           tab_set_panel,
           width = 12
-        ), 
+        ),
         classes = c("box-no-header", "box-with-tabs")
       )
     }) # end renderUI
     
     # update selected tab based on default -ome
     observe({
-      updateTabsetPanel(inputId = "ome_tabs")
+      updateTabsetPanel(inputId = "ome_tabs", selected = default_ome())
     })
     
     # call the server function for each individual ome
     all_plots <- reactiveVal() # initialize
-    observeEvent(shared$chosen_omes, {
-      output_plots <- sapply(shared$chosen_omes, function(ome) {
-        statSummary_Ome_Server(
+    observeEvent(all_omes(), {
+      output_plots <- sapply(all_omes(), function(ome) {
+        templateSingleOme_Ome_Server(
           # TODO: edit inputs to the ome server function, the last 4 may be unnecessary
           id = ome,
           ome = ome,
@@ -122,26 +122,26 @@ statSummary_Tab_Server <- function(id = "statSummaryTab",
           color_map = reactive(custom_colors()[[ome]])
         )
       }, simplify = FALSE)
-      
+    
       all_plots(output_plots) # set reactive value with outputs
     })
     
     return(all_plots)
-  })
-}
+    })
+    }
 
 
 
 # UI for an individual ome
 statSummary_Ome_UI <- function (id, ome) {
-  
+
   ns <- NS(id)
-  
+
   tagList(
-    
+
     # TODO: add UI for each ome tab here. Make sure to use `ns`!
     # Example included below
-    
+
     # example plot
     fluidRow(
       # Dataset information box
@@ -153,7 +153,7 @@ statSummary_Ome_UI <- function (id, ome) {
         width = 6,
         headerBorder = TRUE
       ),
-      
+
       # Data workflow box
       shinydashboardPlus::box(
         tableOutput(ns("workflow_table")),
@@ -163,7 +163,7 @@ statSummary_Ome_UI <- function (id, ome) {
         width = 6,
         headerBorder = TRUE
       ),
-      
+
       # # P.val histogram box
       # fluidRow(shinydashboardPlus::box(
       #   plotlyOutput(ns("quant_features_plot")),
@@ -180,7 +180,7 @@ statSummary_Ome_UI <- function (id, ome) {
       #   headerBorder = TRUE,
       #   solidHeader = TRUE
       # ))
-      
+
     ) # end fluidRow
   )
 }
@@ -194,15 +194,15 @@ statSummary_Ome_Server <- function(id,
                                  GCT_original,
                                  default_annotation_column,
                                  color_map) {
-  
+
   ## module function
   moduleServer(id, function (input, output, session) {
-    
+
     # get namespace, use in renderUI-like functions
     ns <- session$ns
-    
+
     ## DATASET INFO ##
-    
+
     # render dataset info
     output$dataset_table <- renderTable(
       data.frame(
@@ -210,9 +210,9 @@ statSummary_Ome_Server <- function(id,
         Count = c(nrow(stat.results), sum(stat.results$significant == TRUE))
       )
     )
-    
+
     ## WORKFLOW INFO ##
-    
+
     # render workflow info
     output$workflow_table <- renderTable(
       data.frame(
@@ -220,11 +220,11 @@ statSummary_Ome_Server <- function(id,
         Count = c(input['statSetupTab-select_test'], 10)#replace 10 when I find out if the cutoff number should change as the user adjusts it on this page or whether this is just 0.05
       )
     )
-    
+
     ## P.value Histogram- for both non-adj and adj p values ##
     # Get hist. code from Natalie
-    
-    
+
+
     ## COMPILE EXPORTS ##
     # stat_summary_csv_export_function <- function(stat.results,dir_name) {
     #   print(dir_name)
@@ -234,9 +234,9 @@ statSummary_Ome_Server <- function(id,
     #     row.names = FALSE
     #   )
     # }
-    # 
+    #
     # return(list(stat_summary= stat_summary_csv_export_function))
-    
-    
+
+
   })
 }
