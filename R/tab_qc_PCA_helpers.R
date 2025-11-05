f426a75617afa2f8e07a5dd5e5528cd9d113e773
Cameron Lian <cliandouble08@gmail.com>
2025-11-05 13:27:29 -0500

    Update depreciated PCA plotting syntax

--- R/tab_qc_PCA_helpers.R
+++ R/tab_qc_PCA_helpers.R
@@ -98,38 +98,38 @@ create_PCA_plot <- function (gct, col_of_interest, ome, custom_color_map = NULL,
   # Create aesthetic mappings based on display preferences
   if (is.null(second_col_of_interest)) {
     # Single variable - use existing logic
-    plot_aes <- aes_string(
-      x = paste0("PC", comp.x),
-      y = paste0("PC", comp.y),
-      color = col_of_interest,
-      text = "tooltip"
+    plot_aes <- aes(
+      x = .data[[paste0("PC", comp.x)]],
+      y = .data[[paste0("PC", comp.y)]],
+      color = .data[[col_of_interest]],
+      text = .data$tooltip
     )
     plot_title <- paste0("PCA plot by ", col_of_interest, ": ", ome)
   } else {
     # Two variables - create appropriate aesthetic mapping
     if (var1_display == "color" && var2_display == "shape") {
-      plot_aes <- aes_string(
-        x = paste0("PC", comp.x),
-        y = paste0("PC", comp.y),
-        color = col_of_interest,
-        shape = second_col_of_interest,
-        text = "tooltip"
+      plot_aes <- aes(
+        x = .data[[paste0("PC", comp.x)]],
+        y = .data[[paste0("PC", comp.y)]],
+        color = .data[[col_of_interest]],
+        shape = .data[[second_col_of_interest]],
+        text = .data$tooltip
       )
     } else if (var1_display == "shape" && var2_display == "color") {
-      plot_aes <- aes_string(
-        x = paste0("PC", comp.x),
-        y = paste0("PC", comp.y),
-        color = second_col_of_interest,
-        shape = col_of_interest,
-        text = "tooltip"
+      plot_aes <- aes(
+        x = .data[[paste0("PC", comp.x)]],
+        y = .data[[paste0("PC", comp.y)]],
+        color = .data[[second_col_of_interest]],
+        shape = .data[[col_of_interest]],
+        text = .data$tooltip
       )
     } else {
       # Default fallback
-      plot_aes <- aes_string(
-        x = paste0("PC", comp.x),
-        y = paste0("PC", comp.y),
-        color = col_of_interest,
-        text = "tooltip"
+      plot_aes <- aes(
+        x = .data[[paste0("PC", comp.x)]],
+        y = .data[[paste0("PC", comp.y)]],
+        color = .data[[col_of_interest]],
+        text = .data$tooltip
       )
     }
     plot_title <- paste0("PCA plot by ", col_of_interest, " and ", second_col_of_interest, ": ", ome)
