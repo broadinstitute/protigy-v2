b6f351302bf5bb855477d7b66e27979c00f04c7e
Cameron Genxuan Lian <glian@broadinstitute.org>
2026-07-01 16:25:59 -0400

    test(pelsa): assert volcano export capitalizes feature-coloring subtitle

--- tests/testthat/test-pelsa-export-aesthetics.R
+++ tests/testthat/test-pelsa-export-aesthetics.R
@@ -14,3 +14,19 @@ test_that("volcano export subtitle capitalizes the word after the pipe", {
                             volcano_label = "All-peptide volcano")
   expect_equal(g$labels$subtitle, "All-peptide volcano | Significance coloring")
 })
+
+test_that("volcano export subtitle capitalizes feature coloring mode", {
+  df <- data.frame(
+    logFC                = c(-1, 0, 1),
+    logP                 = c(1, 0.2, 1.5),
+    sig_direction        = c("down", "ns", "up"),
+    feature_class_primary = c("none", "none", "none"),
+    is_marker            = c(FALSE, FALSE, FALSE),
+    label                = c("", "", ""),
+    stringsAsFactors     = FALSE
+  )
+  attr(df, "y_cutoff") <- 1.0
+  g <- .pelsa_export_ggplot(df, full_df = df, color_mode = "feature",
+                            volcano_label = "All-peptide volcano")
+  expect_equal(g$labels$subtitle, "All-peptide volcano | Feature coloring")
+})
